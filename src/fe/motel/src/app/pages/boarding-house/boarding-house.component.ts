c2d7b7ebfea4f4b6b00615264c4e22e48cd0ab45
nhan1110i <trongnhan1110i@gmail.com>
2022-09-21 21:17:35 +0700

    add boarding ui

--- src/fe/motel/src/app/pages/boarding-house/boarding-house.component.ts
+++ src/fe/motel/src/app/pages/boarding-house/boarding-house.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-boarding-house',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardingHouseComponent implements OnInit {
 
-  constructor() { }
+  provides = [];
+  selectedProvides = [];
+  constructor(
+    private _service : AppService
+  ) { }
 
   ngOnInit(): void {
+    this.getProvides();
   }
+
+  addBoardingHouse(){
+    console.log(this.selectedProvides);
+  }
+
+  getProvides(){
+    this._service.getProvides().subscribe(
+      response =>{
+        this.provides = response.data;
+      }
+    )
+  }
+
+  setSelectProvide(id){
+    let index = this.selectedProvides.indexOf(id);
+    if( index > -1){
+      this.selectedProvides.splice(index,1);
+    }else{
+      this.selectedProvides.push(id);
+    }
+  }
+
+
 
 }
